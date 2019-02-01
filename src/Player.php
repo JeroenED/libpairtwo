dd5dee63e91b01042859338e95d5e9ac34074e2d
Jeroen De Meerleer <me@jeroened.be>
2019-02-01 15:53:39 +0100

    Models in Models, logic in src

---
+++ src/Player.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: jeroen
+ * Date: 1/02/19
+ * Time: 11:26
+ */
+
+namespace JeroenED\LibPairtwo;
+
+use JeroenED\Libpairtwo\Models\Player as PlayerModel;
+
+class Player Extends PlayerModel
+{
+
+}
---
+++ src/Tournament.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: jeroen
+ * Date: 1/02/19
+ * Time: 11:18
+ */
+
+namespace JeroenED\Libpairtwo;
+
+use JeroenED\Libpairtwo\Models\Tournament as TournamentModel;
+
+class Tournament extends TournamentModel
+{
+    /**
+     * @param Integer $id
+     * @return Player
+     */
+    public function getPlayerById($id)
+    {
+        return $this->GetPlayers()[$id];
+    }
+
+    /**
+     * @param Player $Player
+     */
+    public function addPlayer(Player $Player)
+    {
+        $newArray = $this->GetPlayers();
+        $newArray[] = $Player;
+        $this->setPlayers($newArray);
+    }
+}
