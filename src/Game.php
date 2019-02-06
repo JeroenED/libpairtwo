f8b0d35fa1111ac406b21053592f5b1618249446
Jeroen De Meerleer <me@jeroened.be>
2019-02-06 17:23:37 +0100

    Added a reading of rounds
    not finished yet

---
+++ src/Game.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: jeroen
+ * Date: 1/02/19
+ * Time: 17:17
+ */
+
+namespace JeroenED\Libpairtwo;
+
+use JeroenED\Libpairtwo\Models\Game as GameModel;
+
+class Game extends GameModel
+{
+
+}
---
+++ src/Models/Game.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: jeroen
+ * Date: 1/02/19
+ * Time: 17:16
+ */
+
+namespace JeroenED\Libpairtwo\Models;
+
+
+class Game
+{
+
+}
