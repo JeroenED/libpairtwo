3f44bc6f0102c2f893f5a57b758a0400b53d66d5
Jeroen De Meerleer <me@jeroened.be>
2019-03-19 16:47:19 +0100

    Not using literal values for Result

--- src/Enums/Result.php
+++ src/Enums/Result.php
@@ -18,7 +18,7 @@ class Result extends Enum
     const won = '1';
     const absent = '0 FF';
     const wonforfait = '1 FF';
-    const adjourn = '0 A';
+    const adjourned = '0 A';
     const drawadjourned = '0.5 A';
     const wonadjourned = '1 A';
     const bye = '0 Bye';
