c6d1afd171baa048647f12fda9e72fd200e59e98
Jeroen De Meerleer <me@jeroened.be>
2019-03-20 12:46:46 +0100

    Reimplemented creation of results

--- src/Enums/Gameresult.php
+++ src/Enums/Gameresult.php
@@ -12,13 +12,16 @@ use MyCLabs\Enum\Enum;
 
 class Gameresult extends Enum
 {
-    const WhiteWins = "1-0";
-    const WhiteWinsForfait = "1-0FF";
-    const WhiteWinsAdjourned = "1-0A";
-    const BlackWins = "0-1";
-    const BlackWinsForfait = "0-1FF";
-    const BlackWinsAdjourned = "0-1A";
-    const Draw = "0.5-0.5";
-    const DrawAdjourned = "0.5-0.5A";
-    const None = "-";
+    const WhiteWins = '1-0';
+    const Draw = '0.5-0.5';
+    const BlackWins = '0-1';
+    const WhiteWinsForfait = '1-0 FF';
+    const BlackWinsForfait = '0-1 FF';
+    const BothLoseForfait = '0-0 FF';
+    const BothWinAdjourned = '1-1 A';
+    const WhiteWinsBlackDrawsAdjourned = '1-0.5 A';
+    const WhiteDrawsBlackWinsAdjourned = '0.5-1 A';
+    const DrawAdjourned = '0.5-0.5 A';
+    const WhiteLoseBlackDrawsAdjourned = '0-0.5';
+    const WhiteDrawsBlackLoseAdjourned = '0.5-0';
 }
--- src/Game.php
+++ src/Game.php
@@ -8,8 +8,36 @@
 
 namespace JeroenED\Libpairtwo;
 
+use JeroenED\Libpairtwo\Enums\Gameresult;
 use JeroenED\Libpairtwo\Models\Game as GameModel;
 
 class Game extends GameModel
 {
+    /**
+     * This function gets the result from the game
+     */
+    public function getResult(): Gameresult
+    {
+        if (!is_null(parent::getResult())) {
+            return parent::getResult();
+        }
+
+        $whiteResult = $this->getWhite()->getResult();
+        $blackResult = $this->getBlack()->getResult();
+
+        $whitesplit = explode(" ", $whiteResult);
+        $blacksplit = explode(" ", $blackResult);
+
+        $special='';
+        if (isset($whitesplit[1]) && $whitesplit[1] != 'Bye') {
+            $special = ' ' . $whitesplit[1];
+        }
+        if (isset($blacksplit[1]) && $blacksplit[1] != 'Bye') {
+            $special = ' ' . $blacksplit[1];
+        }
+        $result = $whitesplit[0] . '-' . $blacksplit[0] . $special;
+        $this->setResult(new Gameresult($result));
+
+        return new Gameresult($result);
+    }
 }
