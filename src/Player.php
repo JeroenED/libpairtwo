fbe6009bd636e0aa6dd7556873b3367819f56e53
Jeroen De Meerleer <me@jeroened.be>
2019-05-01 16:04:42 +0200

    Merge branch 'task/get-player-by-name' into develop

--- src/Player.php
+++ src/Player.php
@@ -10,6 +10,10 @@ namespace JeroenED\Libpairtwo;
 
 use JeroenED\Libpairtwo\Models\Player as PlayerModel;
 
+/**
+ * Class Player
+ * @package JeroenED\Libpairtwo
+ */
 class Player extends PlayerModel
 {
     
@@ -23,5 +27,29 @@ class Player extends PlayerModel
         $newArray = $this->GetPairings();
         $newArray[] = $pairing;
         $this->setPairings($newArray);
+    }
+
+    /**
+     * Returns an array of Player objects where name matches $search
+     *
+     * @param string $search
+     * @param Tournament $tournament
+     * @return Player[]
+     */
+    public static function getPlayersByName(string $search, Tournament $tournament): array
+    {
+        /** @var Player[] */
+        $players = $tournament->getPlayers();
+
+        /** @var Player[] */
+        $return = [];
+
+        foreach ($players as $player) {
+            if (fnmatch($search, $player->getName())) {
+                $return[] = $player;
+            }
+        }
+
+        return $return;
     }
 }
