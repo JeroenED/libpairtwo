7bfb68da552a92cc018f912bdb99293096e7c242
Jeroen De Meerleer <me@jeroened.be>
2019-05-01 15:49:12 +0200

    Added pairings to round objects

--- src/Round.php
+++ src/Round.php
@@ -9,6 +9,8 @@
 namespace JeroenED\Libpairtwo;
 
 use JeroenED\Libpairtwo\Models\Round as RoundModel;
+use JeroenED\Libpairtwo\Game;
+use JeroenED\Libpairtwo\Pairing;
 
 class Round extends RoundModel
 {
@@ -22,5 +24,17 @@ class Round extends RoundModel
         $newarray = $this->getGames();
         $newarray[] = $game;
         $this->setGames($newarray);
+    }
+
+    /**
+     * Adds a pairing to the round
+     *
+     * @param Pairing $pairing
+     */
+    public function addPairing(Pairing $pairing)
+    {
+        $newarray = $this->getPairings();
+        $newarray[] = $pairing;
+        $this->setPairings($newarray);
     }
 }
