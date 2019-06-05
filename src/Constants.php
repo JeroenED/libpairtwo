154d704f3476ee254aa8b86538f21121430a20ca
Jeroen De Meerleer <me@jeroened.be>
2019-06-05 13:25:14 +0200

    NEW FEATURE: Added possibility to get played games

--- src/Constants.php
+++ src/Constants.php
@@ -9,9 +9,10 @@ use JeroenED\Libpairtwo\Enums\Result;
 class Constants
 {
     const Won = [ Result::won, Result::wonforfait, Result::wonbye, Result::wonadjourned ];
-    const Draw = [ Result::draw, Result::drawadjourned];
+    const Draw = [ Result::draw, Result::drawadjourned ];
     const Lost = [ Result::absent, Result::bye, Result::lost, Result::adjourned ];
     const NotPlayed = [ Result::bye, Result::wonbye, Result::absent ];
+    const Played = [ Result::won, Result::wonforfait, Result::wonbye, Result::wonadjourned, Result::draw, Result::drawadjourned, Result::absent, Result::bye, Result::lost, Result::adjourned ];
     const Black = [ Color::black ];
     const White = [ Color::white ];
 }
