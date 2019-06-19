e054d4af870ee34b7d5128fcedcec3e06615c83e
Jeroen De Meerleer <me@jeroened.be>
2019-06-19 21:54:19 +0200

    MAJOR CHANGE: Model Classes has been removed
    CHANGE: Replaced PhpDoc with Doxygen

--- src/Interfaces/ReaderInterface.php
+++ src/Interfaces/ReaderInterface.php
@@ -1,12 +1,43 @@
 <?php
-
+/**
+ * Interface ReaderInterface
+ *
+ * Sets the methods a reader needs to implement
+ *
+ * @author      Jeroen De Meerleer <schaak@jeroened.be>
+ * @category    Main
+ * @package     Libpairtwo
+ * @copyright   Copyright (c) 2018-2019 Jeroen De Meerleer <schaak@jeroened.be>
+ */
 
 namespace JeroenED\Libpairtwo\Interfaces;
 
 use JeroenED\Libpairtwo\Tournament;
 
+/**
+ * Interface ReaderInterface
+ *
+ * Sets the methods a reader needs to implement
+ *
+ * @author      Jeroen De Meerleer <schaak@jeroened.be>
+ * @category    Main
+ * @package     Libpairtwo
+ * @copyright   Copyright (c) 2018-2019 Jeroen De Meerleer <schaak@jeroened.be>
+ */
 interface ReaderInterface
 {
-    public function read($filename): ReaderInterface;
+    /**
+     * Reads out $filename
+     *
+     * @param $filename
+     * @return ReaderInterface
+     */
+    public function read(string $filename): ReaderInterface;
+
+    /**
+     * Gets the tournament out of $filename
+     *
+     * @return Tournament
+     */
     public function getTournament(): Tournament;
 }
