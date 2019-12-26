7826b2d3af1a22945d77ebaa7c43aeca5c87dfcd
Jeroen De Meerleer <me@jeroened.be>
2019-12-26 20:00:36 +0100

    META: Documentation fixes

--- src/Player.php
+++ src/Player.php
@@ -348,7 +348,8 @@ class Player
      *
      * @return Player[]
      */
-    private function opponents() {
+    private function opponents()
+    {
         $return = array();
         foreach ($this->Pairings as $pairing) {
             if (!empty($pairing->Opponent)) {
